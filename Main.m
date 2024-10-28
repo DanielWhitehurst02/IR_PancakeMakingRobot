4ac72ef4f768b01ea1af2cc04dd0c6fdb498573a
DesmoKg <homedecoreline@gmail.com>
2024-10-29 10:42:11 +1100

    changes

--- Main.m
+++ Main.m
@@ -150,6 +150,20 @@ function Main(app)
         pause(0.1);
         hold on;
 
+
+        %n = input('Enter a number: ');
+
+        %switch n
+        %    case -1
+        %        disp('negative one')
+        %    case 0
+        %        disp('zero')
+        %    case 1
+        %        disp('positive one')
+        %    otherwise
+        %        disp('other value')
+        %end
+
     end
 
     % If the loop has exited
