cc66d26a7b9041171c9f6855a24a80e39bbd16b2
DesmoKg <homedecoreline@gmail.com>
2024-10-30 09:13:10 +1100

    new objects

--- Main.m
+++ Main.m
@@ -1,93 +1,82 @@
 function Main(app)
-    
-    %%Setting up workspace
-    disp('Loading the environemnt');
-    workspace = [-2,3,-3,2.5,0,2];
-    hold on
-    axis equal
-    axis(workspace)
-    surf([-2.5,-2.5;3.5,3.5] ...
-    ,[-2.5,2.5;-2.5,2.5] ...
-    ,[0.01,0.01;0.01,0.01] ...
-    ,'CData',imread('florring.jpg') ...
-    ,'FaceColor','texturemap');
-    
-    %defining pickable objects variables
-    BlueBottle=[0.8,1.25,0.88];
-    PinkBottle=[1.05,1.25,0.88];
-    BrownBottle=[1.3,1.25,0.88];
-    DoughBottle=[0.6,1.25,0.88];
-    Spatula=[-0.42,0.5,0.95];
-    
-    %Place other objects
-    
-      % Place the object
-    mesh_hBlue = PlaceObject('BlueSyrupBottle.ply');
-    verticesBlue = get(mesh_hBlue, 'Vertices');
-    transformedVertices = [verticesBlue, ones(size(verticesBlue, 1), 1)] * transl(BlueBottle)';
-    set(mesh_hBlue, 'Vertices', transformedVertices(:, 1:3)); % Update object position
-    
-    mesh_hPink = PlaceObject('PinkSyrupBottle.ply');
-    vertices = get(mesh_hPink, 'Vertices');
-    transformedVertices = [vertices, ones(size(vertices, 1), 1)] * transl(PinkBottle)';
-    set(mesh_hPink, 'Vertices', transformedVertices(:, 1:3)); % Update object position
-    
-    mesh_hBrown = PlaceObject('BrownSyrupBottle.ply');
-    verticesPink = get(mesh_hBrown, 'Vertices');
-    transformedVertices = [verticesPink, ones(size(verticesPink, 1), 1)] * transl(BrownBottle)';
-    set(mesh_hBrown, 'Vertices', transformedVertices(:, 1:3)); % Update object position
-    
-    mesh_hDough = PlaceObject('DoughBottle.ply');
-    verticesDough = get(mesh_hDough, 'Vertices');
-    transformedVertices = [verticesDough, ones(size(verticesDough, 1), 1)] * transl(DoughBottle)';
-    set(mesh_hDough, 'Vertices', transformedVertices(:, 1:3)); % Update object position
-    
-    mesh_hSpatula = PlaceObject('spatula.ply');
-    verticesSpatula = get(mesh_hSpatula, 'Vertices');
-    transformedVertices = [verticesSpatula, ones(size(verticesSpatula, 1), 1)] * transl(Spatula)';
-    set(mesh_hSpatula, 'Vertices', transformedVertices(:, 1:3)); % Update object position
-    
-    %mesh_hPancake = PlaceObject('pancake2.ply');
-    %verticesPancake = get(mesh_hPancake, 'Vertices');
-    %transformedVertices = [verticesPancake, ones(size(verticesPancake, 1), 1)] * transl([-0.3, -0.33, 0.57])';
-    %set(mesh_hPancake, 'Vertices', transformedVertices(:, 1:3)); % Update object position
-    
-    PlaceObject('Stove.ply',[-0.2,0.5,0.5]);
-    PlaceObject('ConveyorBelt.ply',[0.2,-0.5,0]);
-    PlaceObject('Table.ply',[-1.7,0.5,-0.15]);
-    PlaceObject('Table.ply',[1.6,0.5,-0.15]);
-    PlaceObject('Shelf.ply',[1,1.3,0.88]);
-    PlaceObject('tableBrown2.1x1.4x0.5m.ply',[-0.45,0.78,0]);
-    PlaceObject('tableBrown2.1x1.4x0.5m.ply',[0.7,0.78,0]);
-    PlaceObject('SpatulaHolder.ply',[-0.4,0.7,0.52]);
-    PlaceObject('FireExtinguisherElevated.ply',[-1.5,-0.95,0.5]);
-    PlaceObject('Plate.ply', [-0.3, -0.33, 0.565]);
-    PlaceObject('emergencyStopButton.ply',[0,-1.7,0]);
-    PlaceObject('glass1.ply', [2.12,1.85,1.8]);
-    PlaceObject('glass2.ply', [-1.9,1.85,1.8]);
-    PlaceObject('glass3.ply', [2.1,1.85,1.8]);
-    PlaceObject('barrier1.5x0.2x1m.ply',[0.8,-1.3,0]);
-    PlaceObject('barrier1.5x0.2x1m.ply',[-0.5,-1.3,0]);
-    
-    
-    time = 4;  % Total time for movement (in seconds)
-    deltaT = 0.2;  % Control step time (in seconds
-    
-    
-    for position = [1.6,1.55,0; 1.6,-1.2,0;]'
-        h = PlaceObject('barrier1.5x0.2x1m.ply', position');
-        verts = [get(h,'Vertices'), ones(size(get(h,'Vertices'),1),1)] * trotz(pi/2);
-        set(h,'Vertices',verts(:,1:3))
-    end
-    
-    for position = [-1.67,0.15,-0.15]'
-        h = PlaceObject('Table.ply', position');
-        verts = [get(h,'Vertices'), ones(size(get(h,'Vertices'),1),1)] * trotz(pi/2);
-        set(h,'Vertices',verts(:,1:3))
-    end
-    
-    %% Initialization Robot
-    robot = UR3e(transl(-0.5,0.2,0.5));
+
+    %% Setting up workspace
+workspace = [-2,3,-3,2.5,0,2];
+hold on
+axis equal
+axis(workspace)
+surf([-2.5,-2.5;3.5,3.5] ...
+,[-2.5,2.5;-2.5,2.5] ...
+,[0.01,0.01;0.01,0.01] ...
+,'CData',imread('florring.jpg') ...
+,'FaceColor','texturemap');
+
+%defining pickable objects variables
+BlueBottle=[0.8,1.25,0.82];
+PinkBottle=[1.05,1.25,0.82];
+BrownBottle=[1.3,1.25,0.82];
+DoughBottle=[0.6,1.25,0.82];
+Spatula=[-0.42,0.5,0.95];
+
+%Place other objects
+
+  % Place the object
+mesh_hBlue = PlaceObject('BlueSyrupBottle.ply');
+verticesBlue = get(mesh_hBlue, 'Vertices');
+transformedVertices = [verticesBlue, ones(size(verticesBlue, 1), 1)] * transl(BlueBottle)';
+set(mesh_hBlue, 'Vertices', transformedVertices(:, 1:3)); % Update object position
+
+mesh_hPink = PlaceObject('PinkSyrupBottle.ply');
+vertices = get(mesh_hPink, 'Vertices');
+transformedVertices = [vertices, ones(size(vertices, 1), 1)] * transl(PinkBottle)';
+set(mesh_hPink, 'Vertices', transformedVertices(:, 1:3)); % Update object position
+
+mesh_hBrown = PlaceObject('BrownSyrupBottle.ply');
+verticesPink = get(mesh_hBrown, 'Vertices');
+transformedVertices = [verticesPink, ones(size(verticesPink, 1), 1)] * transl(BrownBottle)';
+set(mesh_hBrown, 'Vertices', transformedVertices(:, 1:3)); % Update object position
+
+mesh_hDough = PlaceObject('DoughBottle.ply');
+verticesDough = get(mesh_hDough, 'Vertices');
+transformedVertices = [verticesDough, ones(size(verticesDough, 1), 1)] * transl(DoughBottle)';
+set(mesh_hDough, 'Vertices', transformedVertices(:, 1:3)); % Update object position
+
+mesh_hSpatula = PlaceObject('spatula.ply');
+verticesSpatula = get(mesh_hSpatula, 'Vertices');
+transformedVertices = [verticesSpatula, ones(size(verticesSpatula, 1), 1)] * transl(Spatula)';
+set(mesh_hSpatula, 'Vertices', transformedVertices(:, 1:3)); % Update object position
+
+
+
+PlaceObject('Stove.ply',[-0.2,0.5,0.5]);
+PlaceObject('ConveyorBelt.ply',[0.2,-0.5,0]);
+PlaceObject('Table.ply',[-1.7,0.5,-0.15]);
+PlaceObject('Table.ply',[1.6,0.5,-0.15]);
+PlaceObject('Shelf.ply',[1,1.3,0.52]);
+PlaceObject('tableBrown2.1x1.4x0.5m.ply',[-0.45,0.78,0]);
+PlaceObject('tableBrown2.1x1.4x0.5m.ply',[0.7,0.78,0]);
+PlaceObject('SpatulaHolder.ply',[-0.4,0.7,0.52]);
+PlaceObject('FireExtinguisherElevated.ply',[-1.5,-0.95,0.5]);
+PlaceObject('Plate.ply', [-0.3, -0.33, 0.565]);
+PlaceObject('emergencyStopButton.ply',[0,-1.7,0]);
+PlaceObject('glass1.ply', [2.12,1.85,1.8]);
+PlaceObject('glass2.ply', [-1.9,1.85,1.8]);
+PlaceObject('glass3.ply', [2.1,1.85,1.8]);
+PlaceObject('barrier1.5x0.2x1m.ply',[0.8,-1.3,0]);
+PlaceObject('barrier1.5x0.2x1m.ply',[-0.5,-1.3,0]);
+for position = [1.6,1.55,0; 1.6,-1.2,0;]'
+    h = PlaceObject('barrier1.5x0.2x1m.ply', position');
+    verts = [get(h,'Vertices'), ones(size(get(h,'Vertices'),1),1)] * trotz(pi/2);
+    set(h,'Vertices',verts(:,1:3))
+end
+
+for position = [-1.67,0.15,-0.15]'
+    h = PlaceObject('Table.ply', position');
+    verts = [get(h,'Vertices'), ones(size(get(h,'Vertices'),1),1)] * trotz(pi/2);
+    set(h,'Vertices',verts(:,1:3))
+end
+    %%Initialization Robot
+    robot = UR3(transl(-0.5,0.2,0.5));
     % robot2 = UR3e(transl(0.5, 0.5, 0));
     leftFinger = Finger();  % Initialize left finger
     rightFinger = Finger2();  % Initialize right finger
@@ -108,7 +97,7 @@ function Main(app)
              0.04, 0.045, 0.125; 
              0.0, 0.0, 0.0;];
 
-    robot2 = Panda(transl(0.9,0.5,0.5));
+    robot2 = Panda(transl(0.9, 0.5, 0.5));
 
 
     centerPoints2 = [
@@ -144,7 +133,7 @@ function Main(app)
     %[cubePoints, vertex, faces, faceNormals,redHandle] = CollisionMesh(0.5, 0.5, rot, 0.02, center);
     %delete(redHandle);
     cubePoints = [];
-
+    cubePoints2=[];
     %% Create Motion Handler
     % Set the total time and control frequency
     t = 5;  % Total time for movement (in seconds)
@@ -171,25 +160,188 @@ function Main(app)
     
     %% Calling Motion handler
     motionHandler1 = MotionHandlerWIthGripperAndObjects(robot, centerPoints, radii, cubePoints, leftFinger, rightFinger,app);
-        
-    %%GripperAttachedPositionsValues = [troty(-pi/2) * trotz(-pi),transl(0,0.15,0)]
-    motionHandler2 = MotionHandlerWIthGripperAndObjects(robot2, centerPoints2, radii2, cubePoints, leftFinger2, rightFinger2,troty(-pi/2) * trotz(-pi),transl(0,0.15,0),app);
+
+    motionHandler2 = MotionHandlerWIthGripperAndObjects(robot2, centerPoints2, radii2, cubePoints2, leftFinger2, rightFinger2,app,troty(-pi/2) * trotz(-pi),transl(0,0.15,0));
 
     
     %% Start the simulation loop
     i = 0;
     while app.isRunning
        i = i+1;
-        %moving the UR3 robot first
-        endTr = transl(-0.42,0.5,0.95)*trotx(-pi/2) % Example target transformation
-        motionHandler1.runRMRC(endTr, time, deltaT);
-        motionHandler1.OpenOrCloseGrippers('open',50);
-        motionHandler1.OpenOrCloseGrippers('close',50);
+                    %% First
+                endTr = transl(0.61,1.1,1.1); % Example target transformation
+                time = 10;  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT);
+                %% Second
+                endTr = transl(0.61,1.1,0.95)*troty(pi/2)*trotx(pi);
+                motionHandler2.runRMRC(endTr,time,deltaT);
+                delete(mesh_hDough); %% We delete the previous one
+                mesh_hDough = PlaceObject('DoughBottle2.ply');
+                verticesDough = get(mesh_hDough, 'Vertices')
+                transformedVertices = [verticesDough, ones(size(verticesDough, 1), 1)] * transl([0.6,1.25,0.82])';
+                set(mesh_hDough, 'Vertices', transformedVertices(:, 1:3)); 
 
-    end
+                motionHandler2.OpenOrCloseGrippers('close',50);
+                %% Third
+                endTr = transl(-0.2,0.8,0.95)*troty(pi/2);
+                  % Total time for movement (in seconds)
+                deltaT = 0.01;
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hDough,verticesDough);
+                %% Fourth
+                % Total time for movement (in seconds)
+                deltaT = 0.05;
+                endTr = transl(-0.2,0.2,0.95)*troty(pi/2);
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hDough,verticesDough);
+                %% Fifth (Dough Pouring)
+                  % Total time for movement (in seconds)
+                deltaT = 0.1;
+                endTr = transl(-0.2,0.2,0.95)*troty(-pi/2)*trotx(-pi/2);
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hDough,verticesDough);
+                
+                % Place the pancake at an initial position after the dough
+                mesh_hPancake = PlaceObject('pancake2.ply');
+                verticesPancake = get(mesh_hPancake, 'Vertices');
+                transformedVertices = [verticesPancake, ones(size(verticesPancake, 1), 1)] * transl([0.1, 0.2, 0.7])';
+                set(mesh_hPancake, 'Vertices', transformedVertices(:, 1:3)); % Update pancake's initial position
+                
+                 %% Sixth 
+                  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                endTr = transl(-0.2,0.2,0.95)*troty(pi/2);
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hDough,verticesDough);
+                 %% Seventh
+                endTr = transl(0.61,1,0.95)*troty(pi/2);
+                 % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hDough,verticesDough);
+                 %% Eight
+                endTr = transl(0.61,1,0.95)*troty(pi/2)*trotx(pi);
+                 % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hDough,verticesDough);
+                motionHandler2.OpenOrCloseGrippers('open',50);
+            
+                 %% Ninth
+                endTr = transl(0.61,0.9,0.95)*troty(pi/2)*trotx(pi);
+                 % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT);
+                endTr0 = transl(-0.42,0.5,0.95)*trotx(-pi/2) % Example target transformation
+                %% Pancake Animation (UR3)
+                deltaT=0.1
+                motionHandler1 = MotionHandlerWIthGripperAndObjects(robot, centerPoints, radii, cubePoints, leftFinger, rightFinger,app);
+                motionHandler1.runRMRC(endTr0, time, deltaT);
+                motionHandler1.OpenOrCloseGrippers('open',50);
+                motionHandler1.OpenOrCloseGrippers('close',50);
+                endTr1 = transl(-0.2,0.2,0.9)*troty(pi/2) % Example target transformation 
+                motionHandler1.runRMRC(endTr1,time,deltaT,mesh_hSpatula,verticesSpatula);
+                endTr2 = transl(-0.2,0.2,0.8)*troty(pi/2)*trotz(-pi/2); 
+                motionHandler1.runRMRC(endTr2,time,deltaT,mesh_hSpatula,verticesSpatula);
+                endTr3 = transl(-0.1,0.2,0.8)*troty(pi/2)*trotz(-pi/2);
+                motionHandler1.runRMRC(endTr3,time,deltaT,mesh_hSpatula,verticesSpatula);
+                endTr4 = transl(-0.1,0.2,1)*troty(pi/2)*trotz(-pi/2);
+                
+            
+            
+                
+                
+               %% we attach the pancake to the gripper
+                motionHandler1.runRMRC(endTr4,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+               
+                
+                % Move robot away, leaving pancake at the drop position
+                endTr5 = transl(-0.1, 0.2, 0.9) * troty(pi/2) * trotz(pi/2);
+                motionHandler1.runRMRC(endTr5, time, deltaT, mesh_hSpatula, verticesSpatula,mesh_hPancake,verticesPancake );
+                
+                
+                %Set new location for the pancake
+                delete(mesh_hPancake); %% We delete the previous one
+                mesh_hPancake = PlaceObject('pancake2.ply');
+                verticesPancake = get(mesh_hPancake, 'Vertices')
+                transformedVertices = [verticesPancake, ones(size(verticesPancake, 1), 1)] * transl([0.1, 0.2, 0.7])';
+                set(mesh_hPancake, 'Vertices', transformedVertices(:, 1:3)); % Update pancake's initial position
+            
+            
+                endTr6 = transl(-0.1,0.2,0.8)*troty(pi/2)*trotz(pi/2);
+                motionHandler1.runRMRC(endTr6,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+                endTr7 = transl(-0.2,0.2,0.9)*troty(pi/2)*trotz(-pi/2);
+                motionHandler1.runRMRC(endTr7,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+                endTr8 = transl(-0.2,0.2,0.8)*troty(pi/2)*trotz(-pi/2); 
+                motionHandler1.runRMRC(endTr8,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+                endTr9 = transl(-0.1,0.2,0.8)*troty(pi/2)*trotz(-pi/2);
+                motionHandler1.runRMRC(endTr9,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+                endTr10 = transl(-0.1,0.2,0.9)*troty(pi/2)*trotz(-pi/2);
+                motionHandler1.runRMRC(endTr10,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+                endTr11 = transl(-0.3,-0.2,0.8)*troty(pi/2)*trotz(-pi/2);
+                motionHandler1.runRMRC(endTr11,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+                endTr12 = transl(-0.3,-0.2,0.8)*troty(pi/2)*trotz(-pi/2); 
+                motionHandler1.runRMRC(endTr12,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+                endTr13 = transl(-0.1,-0.2,0.8)*troty(pi/2)*trotz(pi/2);
+                motionHandler1.runRMRC(endTr13,time,deltaT,mesh_hSpatula,verticesSpatula,mesh_hPancake,verticesPancake);
+                
+                %Set new location for the pancake
+                delete(mesh_hPancake); %% We delete the previous one
+                mesh_hPancake = PlaceObject('pancake2.ply');
+                verticesPancake = get(mesh_hPancake, 'Vertices')
+                transformedVertices = [verticesPancake, ones(size(verticesPancake, 1), 1)] * transl([-0.3, -0.33, 0.57])';
+                set(mesh_hPancake, 'Vertices', transformedVertices(:, 1:3)); % Update pancake's initial position
 
-    % If the loop has exited
-    disp('Simulation Stopped.');
+      
+                % Define the target transformation matrix
+                 %% Tenth (Syrup)
+                endTr = transl(0.8,1.1,0.95)*troty(pi/2)*trotx(pi);
+                t = 10;  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT);
+                motionHandler2.OpenOrCloseGrippers('close',50);
+               
+                %% Eleventh
+                 endTr = transl(0.8,0,0.95);
+                t = 10;  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hBlue,verticesBlue);
+              
+            
+                %% Twelfth
+                endTr = transl(0.8,-0.9,0.95);
+                t = 10;  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hBlue,verticesBlue);
+                %% Thirteenth
+                endTr = transl(0.8,-0.9,0.95)*troty(-pi/2);
+                t = 10;  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hBlue,verticesBlue);
+            
+                %% Fourteenth
+                endTr = transl(0.8,0,0.95)*troty(pi/2);
+                t = 10;  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT,mesh_hBlue,verticesBlue);
+                
+                %% Fifteenth
+                 endTr = transl(0.8,0.5,0.95)*troty(pi/2)*trotx(pi);
+                t = 10;  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT);
+                
+               
+                %% Fifteenth
+                 endTr = transl(0.8,1,0.95)*troty(pi/2)*trotx(pi);
+                t = 10;  % Total time for movement (in seconds)
+                deltaT = 0.05;
+                motionHandler2.runRMRC(endTr,time,deltaT);
+                motionHandler2.OpenOrCloseGrippers('open',50);
+                end
+            
+                % If the loop has exited
+                disp('Simulation Stopped.');
+
+                % Enter an infinite "pause" state to keep the loop active without re-running the animation
+                while true
+                    pause(0.1);  % Adjust pause duration as needed to reduce CPU load
+                end
 end
 
 
